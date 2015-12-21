5e2a51eb940ec26ed877de9f380ea13f2bbe33ac
Petr Štipek <p.stipek@email.cz>
2015-12-21 16:17:12 +0100

    Added abstract Configuration

---
+++ src/Rest/AConnection.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Pipas\Rest;
+
+/**
+ * @author Petr Štipek <p.stipek@email.cz>
+ */
+abstract class AConnection implements IConnection
+{
+	/** @var Configuration */
+	private $configuration;
+
+	/**
+	 * AConnection constructor.
+	 * @param Configuration $configuration
+	 */
+	public function __construct(Configuration $configuration)
+	{
+		$this->configuration = $configuration;
+	}
+
+	/**
+	 * @return Configuration
+	 */
+	function getConfiguration()
+	{
+		return $this->configuration;
+	}
+
+}
